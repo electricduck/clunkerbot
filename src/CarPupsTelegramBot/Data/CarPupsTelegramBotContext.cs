8bfed604bb47b5d2dc980948fc1122fa3d071a35
Ducky <ducky@zyr.io>
2018-10-23 03:33:18 +0100

    Add Users to DBContext

--- src/CarPupsTelegramBot/Data/CarPupsTelegramBotContext.cs
+++ src/CarPupsTelegramBot/Data/CarPupsTelegramBotContext.cs
@@ -4,6 +4,7 @@ using CarPupsTelegramBot.Models;
 namespace CarPupsTelegramBot.Data {
     public class CarPupsTelegramBotContext : DbContext {
         public DbSet<FuellyModel> Fuelly { get; set; }
+        public DbSet<UserModel> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             optionsBuilder.UseSqlite("Data Source=bot.db"); // TODO: Add to config
