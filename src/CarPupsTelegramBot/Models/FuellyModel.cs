3dcbd17c2343987d354c0a0881b18ee6f917651e
Ducky <ducky@zyr.io>
2018-10-23 03:24:35 +0100

    Add model for Fuelly

---
+++ src/CarPupsTelegramBot/Models/FuellyModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarPupsTelegramBot.Models {
+    public class FuellyModel {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set;}
+
+        public int FuellyId { get; set; }
+    }
+}
