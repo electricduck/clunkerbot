f3501eebf8f5fe50d9eebf6c37adbaa91b07f479
Ducky <ducky@zyr.io>
2018-10-23 17:11:26 +0100

    Remove Fizzler

--- src/CarPupsTelegramBot/Commands/Fuelly.cs
+++ src/CarPupsTelegramBot/Commands/Fuelly.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using Fizzler;
-using Fizzler.Systems.HtmlAgilityPack;
 using HtmlAgilityPack;
 using CarPupsTelegramBot.Data;
 using CarPupsTelegramBot.Models;
