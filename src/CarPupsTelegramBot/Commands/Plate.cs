2a32c374c273123aa646ef2c37e7687c6b222dc2
Ducky <ducky@zyr.io>
2018-12-02 19:47:59 +0000

    Parse Plate - Add parser for 2001-2051 (Current) plates

--- src/CarPupsTelegramBot/Commands/Plate.cs
+++ src/CarPupsTelegramBot/Commands/Plate.cs
@@ -10,12 +10,14 @@ namespace CarPupsTelegramBot.Commands
 {
     public class Plate
     {
-        public static string Parse(string plate, string location)
+        public static string Parse(string plate, string country)
         {
             try {
                 PlateReturnModel plateReturn;
 
-                switch(location) {
+                plate = plate.ToUpper();
+
+                switch(country) {
                     case "gb":
                         plateReturn = GbPlateUtilities.ParseGbPlate(plate);
                         break;
@@ -23,10 +25,50 @@ namespace CarPupsTelegramBot.Commands
                         return HelpData.GetHelp("parseplate", true);
                 }
 
-                string output = $@"Year: {plateReturn.Year}
-Location: {plateReturn.Location}
-Issue: {plateReturn.Issue}
-Type {plateReturn.Format}";
+                string output = $@"#️⃣ <i>Parse Plate:</i> <code>{plate}</code>
+—
+";
+
+                if(plateReturn.Format == Enums.GbPlateFormat.yr1902) {
+                    output += $@"<b>DVLA Office:</b> {plateReturn.Location}
+<b>Issue No.:</b> {plateReturn.Issue}
+<b>Format:</b> 1902 to 1932";
+                } else if(plateReturn.Format == Enums.GbPlateFormat.yr1932) {
+                    output += $@"<b>DVLA Office:</b> {plateReturn.Location}
+<b>Issue No.:</b> {plateReturn.Issue}
+<b>Format:</b> 1932 to 1963";
+                } else if(plateReturn.Format == Enums.GbPlateFormat.yr1953) {
+                    output += $@"<b>DVLA Office:</b> {plateReturn.Location}
+<b>Issue No.:</b> {plateReturn.Issue}
+<b>Format:</b> 1932 to 1963 (Reversed)";
+                } else if(plateReturn.Format == Enums.GbPlateFormat.suffix) {
+                    output += $@"<b>DVLA Office:</b> {plateReturn.Location}
+<b>Year Reg.:</b> {plateReturn.Year}
+<b>Format:</b> Suffix <i>(1963 to 1982)</i>";
+                } else if(plateReturn.Format == Enums.GbPlateFormat.prefix) {
+                    output += $@"<b>DVLA Office:</b> {plateReturn.Location}
+<b>Year Reg.:</b> {plateReturn.Year}
+<b>Format:</b> Prefix <i>(1983 to 2001)</i>";
+                } else if(plateReturn.Format == Enums.GbPlateFormat.current) {
+                    var specialOutput = "<i>No</i>";
+
+                    if(plateReturn.Type == Enums.GbPlatePost2001Type.Reserved) {
+                        specialOutput = "Reserved";
+                    } else if(plateReturn.Type == Enums.GbPlatePost2001Type.Export) {
+                        specialOutput = "Personal Export";
+                    }
+                    
+                    output += $@"<b>DVLA Office:</b> {plateReturn.Location}
+<b>Year Reg.:</b> {plateReturn.Year}
+<b>Special:</b> {specialOutput}
+<b>Format:</b> Current <i>(2001 to 2051)</i>";
+                }
+
+//Year: {plateReturn.Year} ({plateReturn.Month})
+//Location: {plateReturn.Location}
+//Issue: {plateReturn.Issue}
+//Type: {plateReturn.Type}
+//Format: {plateReturn.Format}";
 
                 return output;
             } catch {
