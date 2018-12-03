e79b6d389b83c167881d4bd9f04cefd88569cff9
Ducky <ducky@zyr.io>
2018-12-03 03:14:52 +0000

    Version 18.8.0

--- src/CarPupsTelegramBot/Data/AppVersion.cs
+++ src/CarPupsTelegramBot/Data/AppVersion.cs
@@ -5,7 +5,7 @@ namespace CarPupsTelegramBot.Data
     class AppVersion
     {
         public static readonly int Major = 18;
-        public static readonly int Minor = 7;
+        public static readonly int Minor = 8;
         public static readonly int Patch = 0;
 
         public static readonly string FullVersion = $"{Major}.{Minor}.{Patch}";
