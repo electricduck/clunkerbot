2ed052bb2fba7d8b74215db09868be7a89d7119f
Ducky <ducky@zyr.io>
2018-12-03 03:14:22 +0000

    Fix incorrect help

--- src/CarPupsTelegramBot/Data/HelpData.cs
+++ src/CarPupsTelegramBot/Data/HelpData.cs
@@ -31,7 +31,7 @@ namespace CarPupsTelegramBot.Data
 
         public static string Plate_Parse = @"<code>/parseplate</code>";
 
-        public static string ZeroToSixty_Calculate = @"<code>/calculate0to60 &lt;1&gt; &lt;2&gt; &lt;3&gt; &lt;4&gt; &lt;[5]&gt;</code>
+        public static string ZeroToSixty_Calculate = @"<code>/calculate0to60 &lt;1&gt; &lt;2&gt; &lt;3&gt; &lt;4&gt;</code>
 —
 <code>&lt;1&gt;</code> <b>Power</b> - Vehicle power at the flywheel, with optional unit (<b>hp</b>, <b>ps</b>, or <b>kw</b> -- defaults to <b>hp</b> with no unit) <i>(e.g. 64hp, 123ps, 250kw, 329)</i>
 <code>&lt;2&gt;</code> <b>Weight</b> - Curb weight of vehicle, with optional unit (<b>lbs</b>, or <b>kg</b> -- defaults to <b>kg</b> with no unit) <i>(e.g. 850kg, 2100lbs, 1024)</i>
