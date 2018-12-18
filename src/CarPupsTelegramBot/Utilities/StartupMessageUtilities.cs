ca84780b8e8ddac3c541301b0af0575fca003f04
Ducky <ducky@zyr.io>
2018-12-18 01:19:31 +0000

    Modify startup figlet

--- src/CarPupsTelegramBot/Utilities/StartupMessageUtilities.cs
+++ src/CarPupsTelegramBot/Utilities/StartupMessageUtilities.cs
@@ -6,12 +6,12 @@ namespace CarPupsTelegramBot.Utilities
 {
     class StartupMessageUtlities
     {
-        public static string Figlet = @"  ____ ____ _____ ____  
- / ___|  _ |_   _| __ ) 
-| |   | |_) || | |  _ \ 
-| |___|  __/ | | | |_) |
- \____|_|    |_| |____/ 
-========================";
+        public static string Figlet = @"  ____ _             _             ____        _   
+ / ___| |_   _ _ __ | | _____ _ __| __ )  ___ | |_ 
+| |   | | | | | '_ \| |/ / _ | '__|  _ \ / _ \| __|
+| |___| | |_| | | | |   |  __| |  | |_) | (_) | |_ 
+ \____|_|\__,_|_| |_|_|\_\___|_|  |____/ \___/ \__|
+===================================================";
 
         public static void GenerateStartupMessage()
         {
@@ -21,7 +21,7 @@ namespace CarPupsTelegramBot.Utilities
 
             Console.ForegroundColor = ConsoleColor.Gray;
 
-            Console.WriteLine($"CarPupsTelegramBot. Version {AppVersion.Major}.{AppVersion.Minor}.{AppVersion.Patch}.");
+            Console.WriteLine($"ClunkerBot. Version {AppVersion.Major}.{AppVersion.Minor}.{AppVersion.Patch}.");
             Console.WriteLine($"Written by ElectricDuck. Licensed under MIT.");
 
             Console.ForegroundColor = ConsoleColor.White;
