11f8d2e3e546705ee97f5422aeb4be0b2c9f8adf
Ducky <ducky@zyr.io>
2018-12-19 23:10:03 +0000

    Move actual bot stuff into its own file

---
+++ src/ClunkerBot/Bot.cs
@@ -0,0 +1,225 @@
+using System;
+using System.Linq;
+using Telegram.Bot.Args;
+using ClunkerBot.Api;
+using ClunkerBot.Commands;
+using ClunkerBot.Data;
+using ClunkerBot.Models;
+using ClunkerBot.Models.ReturnModels.MessageReturnModels;
+using ClunkerBot.Utilities;
+
+namespace ClunkerBot
+{
+    class Bot
+    {
+        public static int awooCount = 1;
+
+        public static void RunCommand(string command, string[] arguments, MessageEventArgs telegramMessageEvent)
+        {
+            Bot botClass = new Bot();
+
+            UserModel currentTelegramUser = botClass.GetCurrentTelegramUser(telegramMessageEvent);
+                
+            string joinedArguments = String.Join(" ", arguments);
+
+            Console.WriteLine($"{currentTelegramUser.TelegramName}");
+            
+            switch(command) {
+                case "addcar":
+                case "addcartogarage":
+                        string addCarToGarageOutput = "";
+
+                        if(arguments.Length == 11) {
+                            addCarToGarageOutput = Garage.AddCarTo(
+                                currentTelegramUser,
+                                arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6], arguments[7], arguments[8], arguments[9], arguments[10]
+                            );
+                        }
+
+                        MessageApi.SendTextMessage(addCarToGarageOutput, Program.BotClient, telegramMessageEvent);
+                    break;
+                case "awoo":
+                        string awooOutput;
+
+                        if(AppSettings.Config_Awoo_Repeat) {
+                            awooOutput = String.Concat(Enumerable.Repeat(AppSettings.Config_Awoo_Word, awooCount));
+                            awooCount++;
+                        } else {
+                            awooOutput = AppSettings.Config_Awoo_Word;
+                        }
+
+                        MessageApi.SendTextMessage(awooOutput, Program.BotClient, telegramMessageEvent);
+                    break;
+                case "calculate0to60":
+                case "0to60":
+                        string calculate0To60Output = "";
+
+                        if(arguments.Length == 4) {
+                            calculate0To60Output = ZeroToSixty.Calculate(arguments[0], arguments[1], arguments[2], arguments[3]);
+                        } else if(arguments.Length == 5) {
+                            calculate0To60Output = ZeroToSixty.Calculate(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4]);
+                        } else if(arguments.Length == 6) {
+                            calculate0To60Output = ZeroToSixty.Calculate(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5]);
+                        } else if(arguments.Length == 7) {
+                            calculate0To60Output = ZeroToSixty.Calculate(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6]);
+                        } else {
+                            MessageApi.SendTextMessage(HelpData.GetHelp("calculate0to60"), Program.BotClient, telegramMessageEvent);
+                        }
+
+                        MessageApi.SendTextMessage(calculate0To60Output, Program.BotClient, telegramMessageEvent);
+                    break;
+                case "calculatejourneyprice":
+                case "journeyprice":
+                case "tripprice":
+                        string calculateJourneyPriceOutput = "";
+                
+                        if (arguments.Length == 3) {
+                            calculateJourneyPriceOutput = JourneyPrice.Calculate(arguments[0], arguments[1], arguments[2]);
+                        }
+                
+                        MessageApi.SendTextMessage(calculateJourneyPriceOutput, Program.BotClient, telegramMessageEvent);
+                    break;
+                case "findavailableplate":
+                case "findplate":
+                        string findAvailablePlateOutput = "";
+
+                        if(arguments.Length == 1) {
+                            findAvailablePlateOutput = AvailablePlate.Find(arguments[0], "gb");
+                        } else if(arguments.Length == 2) {
+                            findAvailablePlateOutput = AvailablePlate.Find(arguments[0], arguments[1]);
+                        } else {
+                            MessageApi.SendTextMessage(HelpData.GetHelp("findavailableplate"), Program.BotClient, telegramMessageEvent);
+                        }
+
+                        MessageApi.SendTextMessage(findAvailablePlateOutput, Program.BotClient, telegramMessageEvent);
+                    break;
+                case "getcar":
+                case "getcarfromgarage":
+                        ImageMessageReturnModel getCarFromGarageOutput = null;
+
+                        if(arguments.Length == 1) {
+                            getCarFromGarageOutput = Garage.GetCarFrom(arguments[0]);
+                        }
+
+                        if(String.IsNullOrEmpty(getCarFromGarageOutput.PhotoUrl)) {
+                            MessageApi.SendTextMessage(getCarFromGarageOutput.Caption, Program.BotClient, telegramMessageEvent);
+                        } else {
+                            MessageApi.SendPhotoMessage(getCarFromGarageOutput, Program.BotClient, telegramMessageEvent);
+                        }
+                    break;
+                case "getfuelly":
+                case "fuelly":
+                        ImageMessageReturnModel getFuellyOutput = null;
+
+                        if (arguments.Length == 1) {
+                            getFuellyOutput = Fuelly.Get(arguments[0]);
+                        } else if (arguments.Length == 2) {
+                            getFuellyOutput = Fuelly.Get(arguments[0], arguments[1]);
+                        } else {
+                            MessageApi.SendTextMessage(HelpData.GetHelp("getfuelly"), Program.BotClient, telegramMessageEvent);
+                        }
+
+                        MessageApi.SendPhotoMessage(getFuellyOutput, Program.BotClient, telegramMessageEvent);
+                    break;
+                case "getgarage":
+                        string getGarageOutput = "";
+
+                        if(arguments.Length == 1) {
+                            getGarageOutput = Garage.Get(arguments[0]);
+                        } else {
+                            MessageApi.SendTextMessage(HelpData.GetHelp("getgarage"), Program.BotClient, telegramMessageEvent);
+                        }
+
+                        MessageApi.SendTextMessage(getGarageOutput, Program.BotClient, telegramMessageEvent);
+                    break;
+                case "getweather":
+                case "weather":
+                    string getWeatherOutput = "";
+
+                    if(arguments.Length > 0) {
+                        string getWeather_fullDetailsTrigger = " full";
+
+                        var getWeather_RequstedLocation = joinedArguments
+                            .Replace(getWeather_fullDetailsTrigger, String.Empty);
+
+                        // IDEA: Add some way of forcing C/F/K only?
+                        if(joinedArguments.Contains(getWeather_fullDetailsTrigger)) {
+                            getWeatherOutput = Weather.Get(getWeather_RequstedLocation, false);
+                        } else {
+                            getWeatherOutput = Weather.Get(getWeather_RequstedLocation, true);
+                        }
+                    } else {
+                        MessageApi.SendTextMessage(HelpData.GetHelp("getweather"), Program.BotClient, telegramMessageEvent);
+                    }
+
+                    MessageApi.SendTextMessage(getWeatherOutput, Program.BotClient, telegramMessageEvent);
+                    break;
+                case "guessmileage":
+                        string guessMileageOutput = "";
+
+                        if (arguments.Length == 3) {
+                           guessMileageOutput = Mileage.Guess(arguments[0], Convert.ToInt32(arguments[1]), arguments[2]);
+                        } else if (arguments.Length == 4)  {
+                           guessMileageOutput = Mileage.Guess(arguments[0], Convert.ToInt32(arguments[1]), arguments[2], arguments[3]);
+                        }
+
+                        MessageApi.SendTextMessage(guessMileageOutput, Program.BotClient, telegramMessageEvent);
+                    break;
+                case "help":
+                        string helpOutput = "";
+
+                        if(arguments.Length == 0) {
+                            helpOutput = Help.Get();
+                        } else if(arguments.Length == 1) {
+                            helpOutput = Help.Get(arguments[0]);
+                        }
+
+                        MessageApi.SendTextMessage(helpOutput, Program.BotClient, telegramMessageEvent);
+                    break;
+                case "info":
+                        string infoOutput = "";
+                        infoOutput = Info.Get();
+                        MessageApi.SendTextMessage(infoOutput, Program.BotClient, telegramMessageEvent);
+                    break;
+                case "parseplate":
+                        string parsePlateOutput = "";
+
+                        if(arguments.Length == 1) {
+                            parsePlateOutput = Plate.Parse(arguments[0]);
+                        } else if (arguments.Length == 2) {
+                            parsePlateOutput = Plate.Parse(arguments[0], arguments[1]);
+                        } else {
+                            MessageApi.SendTextMessage(HelpData.GetHelp("parseplate"), Program.BotClient, telegramMessageEvent);
+                        }
+
+                        MessageApi.SendTextMessage(parsePlateOutput, Program.BotClient, telegramMessageEvent);
+
+                    break;
+                case "setcarphoto":
+                        string setCarPhotoOutput = "";
+
+                        if(arguments.Length == 2) {
+                            setCarPhotoOutput = Garage.SetCarPhoto(arguments[0], arguments[1]);
+                        }
+
+                        MessageApi.SendTextMessage(setCarPhotoOutput, Program.BotClient, telegramMessageEvent);
+                    break;
+                default:
+                    ConsoleOutputUtilities.WarnConsoleMessage($@"Command '{command}' doesn't exist");
+                    break;
+            }
+        }
+
+        public UserModel GetCurrentTelegramUser(MessageEventArgs telegramMessageEvent)
+        {
+            UserModel user = new UserModel
+            {
+                TelegramId = telegramMessageEvent.Message.From.Id,
+                TelegramUsername = telegramMessageEvent.Message.From.Username,
+                TelegramName = telegramMessageEvent.Message.From.FirstName + " " + telegramMessageEvent.Message.From.LastName
+            };
+
+            return user;
+        }
+    }
+}
--- src/ClunkerBot/Program.cs
+++ src/ClunkerBot/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
 using Telegram.Bot;
 using Telegram.Bot.Args;
-using Telegram.Bot.Types.Enums;
 using ClunkerBot.Api;
 using ClunkerBot.Commands;
 using ClunkerBot.Data;
@@ -19,8 +16,7 @@ namespace ClunkerBot
 {
     class Program
     {
-        public static int awooCount = 1;
-        public static ITelegramBotClient botClient;
+        public static ITelegramBotClient BotClient;
 
         static void Main()
         {
@@ -42,10 +38,10 @@ namespace ClunkerBot
             HelpData.CompileHelpDictionary();
 
             ConsoleOutputUtilities.DoingConsoleMessage("Connecting to Telegram");
-            botClient = new TelegramBotClient(AppSettings.ApiKeys_Telegram);
+            BotClient = new TelegramBotClient(AppSettings.ApiKeys_Telegram);
             
 
-            bool isApiKeyValid = botClient.TestApiAsync().Result;
+            bool isApiKeyValid = BotClient.TestApiAsync().Result;
 
             if(!isApiKeyValid) {
                 ConsoleOutputUtilities.ErrorConsoleMessage("Connection to Telegram unsuccessful: Invalid API key");
@@ -54,12 +50,12 @@ namespace ClunkerBot
                 ConsoleOutputUtilities.OkayConsoleMessage("Connection to Telegram successful");
             }
 
-            string botId = botClient.BotId.ToString();
+            string botId = BotClient.BotId.ToString();
             ConsoleOutputUtilities.InfoConsoleMessage($"ID of bot is {botId}");
 
-            botClient.OnMessage += Bot_OnMessage;
+            BotClient.OnMessage += Bot_OnMessage;
 
-            botClient.StartReceiving();
+            BotClient.StartReceiving();
             ConsoleOutputUtilities.OkayConsoleMessage("Listening for triggers");
             ConsoleOutputUtilities.SeparatorConsoleMessage();
 
@@ -79,227 +75,19 @@ namespace ClunkerBot
                     if(StringUtilities.CountWords(messageText) == 1) {
                         if(HelpData.HelpDictionary.ContainsKey(command)) {
                             string helpOutput = HelpData.GetHelp(command);
-                            MessageApi.SendTextMessage(helpOutput, botClient, e);
+                            MessageApi.SendTextMessage(helpOutput, BotClient, e);
                         } else {
                             arguments = (messageText.Substring(messageText.IndexOf(' ') + 1)).Split(" ");
-                            RunCommand(command, arguments, e);
+                            Bot.RunCommand(command, arguments, e);
                         }
                     } else {
                         arguments = (messageText.Substring(messageText.IndexOf(' ') + 1)).Split(" ");
-                        RunCommand(command, arguments, e);
+                        Bot.RunCommand(command, arguments, e);
                     }
                 } catch (Exception exception) {
                     ConsoleOutputUtilities.ErrorConsoleMessage(exception.ToString());
                 }
             }
-        }
-
-        static void RunCommand(string command, string[] arguments, MessageEventArgs telegramMessageEvent)
-        {
-            Program program = new Program();
-
-            UserModel currentTelegramUser = program.GetCurrentTelegramUser(telegramMessageEvent);
-                
-            string joinedArguments = String.Join(" ", arguments);
-
-            Console.WriteLine($"{currentTelegramUser.TelegramName}");
-            
-            switch(command) {
-                case "addcar":
-                case "addcartogarage":
-                        string addCarToGarageOutput = "";
-
-                        if(arguments.Length == 11) {
-                            addCarToGarageOutput = Garage.AddCarTo(
-                                currentTelegramUser,
-                                arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6], arguments[7], arguments[8], arguments[9], arguments[10]
-                            );
-                        }
-
-                        MessageApi.SendTextMessage(addCarToGarageOutput, botClient, telegramMessageEvent);
-                    break;
-                case "awoo":
-                        string awooOutput;
-
-                        if(AppSettings.Config_Awoo_Repeat) {
-                            awooOutput = String.Concat(Enumerable.Repeat(AppSettings.Config_Awoo_Word, awooCount));
-                            awooCount++;
-                        } else {
-                            awooOutput = AppSettings.Config_Awoo_Word;
-                        }
-
-                        MessageApi.SendTextMessage(awooOutput, botClient, telegramMessageEvent);
-                    break;
-                case "calculate0to60":
-                case "0to60":
-                        string calculate0To60Output = "";
-
-                        if(arguments.Length == 4) {
-                            calculate0To60Output = ZeroToSixty.Calculate(arguments[0], arguments[1], arguments[2], arguments[3]);
-                        } else if(arguments.Length == 5) {
-                            calculate0To60Output = ZeroToSixty.Calculate(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4]);
-                        } else if(arguments.Length == 6) {
-                            calculate0To60Output = ZeroToSixty.Calculate(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5]);
-                        } else if(arguments.Length == 7) {
-                            calculate0To60Output = ZeroToSixty.Calculate(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6]);
-                        } else {
-                            MessageApi.SendTextMessage(HelpData.GetHelp("calculate0to60"), botClient, telegramMessageEvent);
-                        }
-
-                        MessageApi.SendTextMessage(calculate0To60Output, botClient, telegramMessageEvent);
-                    break;
-                case "calculatejourneyprice":
-                case "journeyprice":
-                case "tripprice":
-                        string calculateJourneyPriceOutput = "";
-                
-                        if (arguments.Length == 3) {
-                            calculateJourneyPriceOutput = JourneyPrice.Calculate(arguments[0], arguments[1], arguments[2]);
-                        }
-                
-                        MessageApi.SendTextMessage(calculateJourneyPriceOutput, botClient, telegramMessageEvent);
-                    break;
-                case "findavailableplate":
-                case "findplate":
-                        string findAvailablePlateOutput = "";
-
-                        if(arguments.Length == 1) {
-                            findAvailablePlateOutput = AvailablePlate.Find(arguments[0], "gb");
-                        } else if(arguments.Length == 2) {
-                            findAvailablePlateOutput = AvailablePlate.Find(arguments[0], arguments[1]);
-                        } else {
-                            MessageApi.SendTextMessage(HelpData.GetHelp("findavailableplate"), botClient, telegramMessageEvent);
-                        }
-
-                        MessageApi.SendTextMessage(findAvailablePlateOutput, botClient, telegramMessageEvent);
-                    break;
-                case "getcar":
-                case "getcarfromgarage":
-                        ImageMessageReturnModel getCarFromGarageOutput = null;
-
-                        if(arguments.Length == 1) {
-                            getCarFromGarageOutput = Garage.GetCarFrom(arguments[0]);
-                        }
-
-                        if(String.IsNullOrEmpty(getCarFromGarageOutput.PhotoUrl)) {
-                            MessageApi.SendTextMessage(getCarFromGarageOutput.Caption, botClient, telegramMessageEvent);
-                        } else {
-                            MessageApi.SendPhotoMessage(getCarFromGarageOutput, botClient, telegramMessageEvent);
-                        }
-                    break;
-                case "getfuelly":
-                case "fuelly":
-                        ImageMessageReturnModel getFuellyOutput = null;
-
-                        if (arguments.Length == 1) {
-                            getFuellyOutput = Fuelly.Get(arguments[0]);
-                        } else if (arguments.Length == 2) {
-                            getFuellyOutput = Fuelly.Get(arguments[0], arguments[1]);
-                        } else {
-                            MessageApi.SendTextMessage(HelpData.GetHelp("getfuelly"), botClient, telegramMessageEvent);
-                        }
-
-                        MessageApi.SendPhotoMessage(getFuellyOutput, botClient, telegramMessageEvent);
-                    break;
-                case "getgarage":
-                        string getGarageOutput = "";
-
-                        if(arguments.Length == 1) {
-                            getGarageOutput = Garage.Get(arguments[0]);
-                        } else {
-                            MessageApi.SendTextMessage(HelpData.GetHelp("getgarage"), botClient, telegramMessageEvent);
-                        }
-
-                        MessageApi.SendTextMessage(getGarageOutput, botClient, telegramMessageEvent);
-                    break;
-                case "getweather":
-                case "weather":
-                    string getWeatherOutput = "";
-
-                    if(arguments.Length > 0) {
-                        string getWeather_fullDetailsTrigger = " full";
-
-                        var getWeather_RequstedLocation = joinedArguments
-                            .Replace(getWeather_fullDetailsTrigger, String.Empty);
-
-                        // IDEA: Add some way of forcing C/F/K only?
-                        if(joinedArguments.Contains(getWeather_fullDetailsTrigger)) {
-                            getWeatherOutput = Weather.Get(getWeather_RequstedLocation, false);
-                        } else {
-                            getWeatherOutput = Weather.Get(getWeather_RequstedLocation, true);
-                        }
-                    } else {
-                        MessageApi.SendTextMessage(HelpData.GetHelp("getweather"), botClient, telegramMessageEvent);
-                    }
-
-                    MessageApi.SendTextMessage(getWeatherOutput, botClient, telegramMessageEvent);
-                    break;
-                case "guessmileage":
-                        string guessMileageOutput = "";
-
-                        if (arguments.Length == 3) {
-                           guessMileageOutput = Mileage.Guess(arguments[0], Convert.ToInt32(arguments[1]), arguments[2]);
-                        } else if (arguments.Length == 4)  {
-                           guessMileageOutput = Mileage.Guess(arguments[0], Convert.ToInt32(arguments[1]), arguments[2], arguments[3]);
-                        }
-
-                        MessageApi.SendTextMessage(guessMileageOutput, botClient, telegramMessageEvent);
-                    break;
-                case "help":
-                        string helpOutput = "";
-
-                        if(arguments.Length == 0) {
-                            helpOutput = Help.Get();
-                        } else if(arguments.Length == 1) {
-                            helpOutput = Help.Get(arguments[0]);
-                        }
-
-                        MessageApi.SendTextMessage(helpOutput, botClient, telegramMessageEvent);
-                    break;
-                case "info":
-                        string infoOutput = "";
-                        infoOutput = Info.Get();
-                        MessageApi.SendTextMessage(infoOutput, botClient, telegramMessageEvent);
-                    break;
-                case "parseplate":
-                        string parsePlateOutput = "";
-
-                        if(arguments.Length == 1) {
-                            parsePlateOutput = Plate.Parse(arguments[0]);
-                        } else if (arguments.Length == 2) {
-                            parsePlateOutput = Plate.Parse(arguments[0], arguments[1]);
-                        } else {
-                            MessageApi.SendTextMessage(HelpData.GetHelp("parseplate"), botClient, telegramMessageEvent);
-                        }
-
-                        MessageApi.SendTextMessage(parsePlateOutput, botClient, telegramMessageEvent);
-
-                    break;
-                case "setcarphoto":
-                        string setCarPhotoOutput = "";
-
-                        if(arguments.Length == 2) {
-                            setCarPhotoOutput = Garage.SetCarPhoto(arguments[0], arguments[1]);
-                        }
-
-                        MessageApi.SendTextMessage(setCarPhotoOutput, botClient, telegramMessageEvent);
-                    break;
-                default:
-                    ConsoleOutputUtilities.WarnConsoleMessage($@"Command '{command}' doesn't exist");
-                    break;
-            }
-        }
-
-        public UserModel GetCurrentTelegramUser(MessageEventArgs telegramMessageEvent)
-        {
-            UserModel user = new UserModel
-            {
-                TelegramId = telegramMessageEvent.Message.From.Id,
-                TelegramUsername = telegramMessageEvent.Message.From.Username,
-                TelegramName = telegramMessageEvent.Message.From.FirstName + " " + telegramMessageEvent.Message.From.LastName
-            };
-
-            return user;
         }
 
         public static void SetupApp() {
