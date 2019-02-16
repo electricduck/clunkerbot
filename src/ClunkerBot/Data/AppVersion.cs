7f6a270e503e932ca9ff4a855c7101b3eafe5af1
Ducky <ducky@zyr.io>
2019-02-16 10:10:19 +0000

    Version 19.5.0

--- src/ClunkerBot/Data/AppVersion.cs
+++ src/ClunkerBot/Data/AppVersion.cs
@@ -5,7 +5,7 @@ namespace ClunkerBot.Data
     class AppVersion
     {
         public static readonly int Major = 19;
-        public static readonly int Minor = 4;
+        public static readonly int Minor = 5;
         public static readonly int Patch = 0;
 
         public static readonly string FullVersion = $"{Major}.{Minor}.{Patch}";
