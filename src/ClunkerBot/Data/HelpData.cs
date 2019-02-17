d74548197369525058847828a65019bd15528605
Ducky <ducky@zyr.io>
2019-02-17 23:55:03 +0000

    Help - Modify mdash's to be actual mdash's

--- src/ClunkerBot/Data/HelpData.cs
+++ src/ClunkerBot/Data/HelpData.cs
@@ -30,20 +30,20 @@ To get additional help about the modules below (such as what arguments they acce
         public static string AvailablePlate_Find = @"<code>/findavailableplate &lt;1&gt; &lt;[2]&gt;</code>
 —
 <code>&lt;1&gt;</code> <b>Plate</b> - The plate you wish to request, void of spaces <i>(e.g. AA15BEN, DU68CKY, 93FAR)</i>
-<code>&lt;2&gt;</code> <b>Country</b> <i>(Optional)</i> - The <a href='https://en.wikipedia.org/wiki/ISO_3166-1#Current'>ISO 3166-1</a> country code -- defaults to <b>gb</b> (Great Britain) <i>(e.g. gb, fr, nl)</i>*
+<code>&lt;2&gt;</code> <b>Country</b> <i>(Optional)</i> - The <a href='https://en.wikipedia.org/wiki/ISO_3166-1#Current'>ISO 3166-1</a> country code — defaults to <b>gb</b> (Great Britain) <i>(e.g. gb, fr, nl)</i>*
 
 <b>*</b> <i>Supported: gb.</i>";
 
         public static string Fuelly_Get = @"<code>/getfuelly &lt;1&gt; &lt;[2]&gt;</code>
 —
-<code>&lt;1&gt;</code> <b>Fuelly ID</b> - ID from the profile URL of a car. So, for `http://www.fuelly.com/car/peugeot/106/2002/electricduck/713804`, the ID would be `713804` -- the integer at the end
-<code>&lt;1&gt;</code> <b>Unit</b> <i>(Optional)</i> - Unit to use (<b>us</b> (US MPG), <b>uk</b> (UK/Imperial MPG) -- defaults to <b>us</b>)";
+<code>&lt;1&gt;</code> <b>Fuelly ID</b> - ID from the profile URL of a car. So, for `http://www.fuelly.com/car/peugeot/106/2002/electricduck/713804`, the ID would be `713804` — the integer at the end
+<code>&lt;1&gt;</code> <b>Unit</b> <i>(Optional)</i> - Unit to use (<b>us</b> (US MPG), <b>uk</b> (UK/Imperial MPG) — defaults to <b>us</b>)";
 
         public static string JourneyPrice_Calculate = @"<code>/calculatejourneyprice &lt;1&gt; &lt;2&gt; &lt;3&gt;</code>
 —
-<code>&lt;1&gt;</code> <b>Distance</b> - Distance to cover, with optional unit (<b>mi</b> or <b>km</b> -- defaults to <b>mi</b> with no unit) <i>(e.g. 100mi, 741km, 31)</i>
-<code>&lt;2&gt;</code> <b>MPG</b> - MPG you are able to achieve, with optional unit (<b>mpg</b>*; to force US: <b>usmpg</b>; to force UK/imperial: <b>ukmpg</b> or <b>impmpg</b> -- defaults to <b>mpg</b> with no unit) <i>(e.g. 34.5mpg, 60ukmpg, 45)</i>
-<code>&lt;3&gt;</code> <b>Fuel price</b> - Price of fuel per unit, with optional unit (<b>/L</b> or <b>/G</b>* -- defaults to <b>/L</b> with no unit), and currency** (<b>£</b>, <b>$*</b>, <b>€</b>, etc.) <i>(e.g. £1.15/L, $2.60/G, 1.30)</i>
+<code>&lt;1&gt;</code> <b>Distance</b> - Distance to cover, with optional unit (<b>mi</b> or <b>km</b> — defaults to <b>mi</b> with no unit) <i>(e.g. 100mi, 741km, 31)</i>
+<code>&lt;2&gt;</code> <b>MPG</b> - MPG you are able to achieve, with optional unit (<b>mpg</b>*; to force US: <b>usmpg</b>; to force UK/imperial: <b>ukmpg</b> or <b>impmpg</b> — defaults to <b>mpg</b> with no unit) <i>(e.g. 34.5mpg, 60ukmpg, 45)</i>
+<code>&lt;3&gt;</code> <b>Fuel price</b> - Price of fuel per unit, with optional unit (<b>/L</b> or <b>/G</b>* — defaults to <b>/L</b> with no unit), and currency** (<b>£</b>, <b>$*</b>, <b>€</b>, etc.) <i>(e.g. £1.15/L, $2.60/G, 1.30)</i>
 
 <b>*</b> <i>If you use '/G' and/or '$' in the fuel price, this will use US MPG, not imperial MPG. You can force it by using the units described above.</i>
 <b>**</b> <i>This makes no difference, but presents your currency in the output message for niceness.</i>";
@@ -74,12 +74,12 @@ To get additional help about the modules below (such as what arguments they acce
 
         public static string ZeroToSixty_Calculate = @"<code>/calculate0to60 &lt;1&gt; &lt;2&gt; &lt;3&gt; &lt;4&gt; &lt;[5]&gt; &lt;[6]&gt;</code>
 —
-<code>&lt;1&gt;</code> <b>Power</b> - Vehicle power at the flywheel, with optional unit (<b>hp</b>, <b>ps</b>, or <b>kw</b> -- defaults to <b>hp</b> with no unit) <i>(e.g. 64hp, 123ps, 250kw, 329)</i>
-<code>&lt;2&gt;</code> <b>Weight</b> - Curb weight of vehicle, with optional unit (<b>lbs</b>, or <b>kg</b> -- defaults to <b>kg</b> with no unit) <i>(e.g. 850kg, 2100lbs, 1024)</i>
+<code>&lt;1&gt;</code> <b>Power</b> - Vehicle power at the flywheel, with optional unit (<b>hp</b>, <b>ps</b>, or <b>kw</b> — defaults to <b>hp</b> with no unit) <i>(e.g. 64hp, 123ps, 250kw, 329)</i>
+<code>&lt;2&gt;</code> <b>Weight</b> - Curb weight of vehicle, with optional unit (<b>lbs</b>, or <b>kg</b> — defaults to <b>kg</b> with no unit) <i>(e.g. 850kg, 2100lbs, 1024)</i>
 <code>&lt;3&gt;</code> <b>Drive Type</b> - Drive type of vehicle: either <b>FWD</b>, <b>RWD</b>, or <b>AWD</b>
 <code>&lt;4&gt;</code> <b>Transmission</b> - Transmission of vehicle: either <b>manual/man</b>, <b>automatic/auto</b>, or <b>dct</b> (Dual-Clutch Semi-Automatic)
-<code>&lt;4&gt;</code> <b>Passengers</b> <i>(Optional)</i> - How many passengers are in the vehicle (weighing approx. 85kg each) -- defaults to <b>0</b>, giving dry vehicle weight in the calculation
-<code>&lt;5&gt;</code> <b>Fuel Volume</b> <i>(Optional)</i> - How much fuel is in the vehicle, with optional unit (<b>l</b>, or <b>gal</b> -- defaults to <b>l</b> with no unit) -- defaults to <b>0L</b>, giving dry vehicle weight in the calculation <i>(e.g. 50L, 10gal, 75)</i>
+<code>&lt;4&gt;</code> <b>Passengers</b> <i>(Optional)</i> - How many passengers are in the vehicle (weighing approx. 85kg each) — defaults to <b>0</b>, giving dry vehicle weight in the calculation
+<code>&lt;5&gt;</code> <b>Fuel Volume</b> <i>(Optional)</i> - How much fuel is in the vehicle, with optional unit (<b>l</b>, or <b>gal</b> — defaults to <b>l</b> with no unit) — defaults to <b>0L</b>, giving dry vehicle weight in the calculation <i>(e.g. 50L, 10gal, 75)</i>
 <code>&lt;6&gt;</code> <b>Fuel Type</b> <i>(Optional)</i> - The type of fuel in the vehicle: either <b>petrol</b>, or <b>diesel</b>";
 
         public static void CompileHelpDictionary()
