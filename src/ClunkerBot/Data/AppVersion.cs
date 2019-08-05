7bf3a7cb861f69eb71fe3fc79b3b799a5350b17b
Ducky <ducky@zyr.io>
2019-08-06 00:21:52 +0100

    Version 19.14.0 (DeLorean)

--- src/ClunkerBot/Data/AppVersion.cs
+++ src/ClunkerBot/Data/AppVersion.cs
@@ -5,9 +5,9 @@ namespace ClunkerBot.Data
     class AppVersion
     {
         public static readonly int Major = 19;
-        public static readonly int Minor = 13;
-        public static readonly int Patch = 9;
-        public static readonly string Release = "Calibra";
+        public static readonly int Minor = 14;
+        public static readonly int Patch = 0;
+        public static readonly string Release = "DeLorean";
 
         public static readonly string FullVersion = $"{Major}.{Minor}.{Patch}";
     }
