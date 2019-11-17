6c9e6a1e5d80887321129c78a94ec93aaf8a4527
Ducky <ducky@zyr.io>
2019-11-17 14:33:04 +0000

    Version 19.16.0 (Escort)

--- src/ClunkerBot/Data/AppVersion.cs
+++ src/ClunkerBot/Data/AppVersion.cs
@@ -5,8 +5,8 @@ namespace ClunkerBot.Data
     class AppVersion
     {
         public static readonly int Major = 19;
-        public static readonly int Minor = 15;
-        public static readonly int Patch = 4;
+        public static readonly int Minor = 16;
+        public static readonly int Patch = 0;
         public static readonly string Release = "Escort";
 
         public static readonly string FullVersion = $"{Major}.{Minor}.{Patch}";
