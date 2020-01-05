c148b709666551a641a6395a4164745870b98e07
Ducky <ducky@zyr.io>
2020-01-05 22:26:53 +0000

    Version 20.0.0 (Fortwo)

--- src/ClunkerBot/Data/AppVersion.cs
+++ src/ClunkerBot/Data/AppVersion.cs
@@ -4,9 +4,9 @@ namespace ClunkerBot.Data
 {
     class AppVersion
     {
-        public static readonly int Major = 19;
-        public static readonly int Minor = 16;
-        public static readonly int Patch = 3;
+        public static readonly int Major = 20;
+        public static readonly int Minor = 0;
+        public static readonly int Patch = 0;
         public static readonly string Release = "Escort";
 
         public static readonly string FullVersion = $"{Major}.{Minor}.{Patch}";
