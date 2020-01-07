aef864e95db5ad805cf64439153104cb8059e737
Ducky <ducky@zyr.io>
2020-01-07 00:04:19 +0000

    Version 20.1.3 (Golf)

--- src/ClunkerBot/Data/AppVersion.cs
+++ src/ClunkerBot/Data/AppVersion.cs
@@ -6,7 +6,7 @@ namespace ClunkerBot.Data
     {
         public static readonly int Major = 20;
         public static readonly int Minor = 1;
-        public static readonly int Patch = 2;
+        public static readonly int Patch = 3;
         public static readonly string Release = "Golf";
 
         public static readonly string FullVersion = $"{Major}.{Minor}.{Patch}";
