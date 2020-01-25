d0212e2c09ca4edda9f618a57a6f95a28774d22c
Ducky <ducky@zyr.io>
2020-01-25 21:50:55 +0000

    Carbon Neutral: Various changes

--- src/ClunkerBot/Commands/CarbonNeutral.cs
+++ src/ClunkerBot/Commands/CarbonNeutral.cs
@@ -26,6 +26,8 @@ namespace ClunkerBot.Commands
                 var treesPerYear = Math.Round((normalizedEmissions.Value * normalizedMileage.Value) / treeCo2AbsorbtionPerYearInGrams);
                 var treesPerMonth = Math.Round(treesPerYear / 12);
 
+                var donationAmount_Ecosia = String.Format("{0:#,0}", (treesPerYear*45));
+                var donationAmount_Ecosia_PerDay = String.Format("{0:#,0}", ((treesPerYear*45)/365));
                 var donationAmount_NationalForest = GenerateDonationAmountString(treesPerYear);
                 var donationAmount_PlantATree = GenerateDonationAmountString(treesPerYear, 5);
                 var donationAmount_TeamTrees = GenerateDonationAmountString(treesPerYear);
@@ -33,11 +35,13 @@ namespace ClunkerBot.Commands
 
                 string result = $@"<b>{treesPerYear} trees per year</b> <i>(approx.)</i> will need to be planted to offest your vehicle's emissions.
 
-<b>Donation</b>
-• <code>{donationAmount_NationalForest}</code> | <a href='https://app.etapestry.com/hosted/NationalForestFoundation/PlantTrees.html'>🇺🇸 National Forest</a>
-• <code>{donationAmount_PlantATree}</code> | <a href='https://plantatreefoundation.org/plant-a-tree-campaign/'>🇺🇸 Plant A Tree</a>
-• <code>{donationAmount_TeamTrees}</code> | <a href='https://teamtrees.org/'>🇺🇸 TeamTrees</a>
-• <code>{donationAmount_WoodlandTrust}</code> | <a href='https://www.woodlandtrust.org.uk/protecting-trees-and-woods/campaign-with-us/big-climate-fightback/'>🇬🇧 Woodland Trust</a>
+<b>What you can do</b>
+• Donate <b>{donationAmount_NationalForest}</b> to <a href='https://app.etapestry.com/hosted/NationalForestFoundation/PlantTrees.html'>🇺🇸 National Forest</a>
+• Donate <b>{donationAmount_PlantATree}</b> to <a href='https://plantatreefoundation.org/plant-a-tree-campaign/'>🇺🇸 Plant A Tree</a>
+• Donate <b>{donationAmount_TeamTrees}</b> to <a href='https://teamtrees.org/'>🇺🇸 TeamTrees</a>
+• Donate <b>{donationAmount_WoodlandTrust}</b> to <a href='https://www.woodlandtrust.org.uk/protecting-trees-and-woods/campaign-with-us/big-climate-fightback/'>🇬🇧 Woodland Trust</a>
+• Search <b>{donationAmount_Ecosia}</b> times <i>({donationAmount_Ecosia_PerDay}/day)</i> on <a href='https://www.ecosia.org/'>Ecosia</a>
+• Walk, you lazy ass
 <hr>
 <i>This is how many trees you'd need to plant for your driving to be carbon neutral, assuming the average tree absorbs 21.77kg of CO2 per year.</i>";
 
@@ -49,7 +53,7 @@ namespace ClunkerBot.Commands
 
         private static string GenerateDonationAmountString(double trees, double modifier = 1, string currency = "$")
         {
-            return (currency + String.Format("{0:0.00}", trees*modifier)).PadLeft(8, ' ');
+            return (currency + String.Format("{0:0.00}", trees*modifier));
         }
     }
 }
